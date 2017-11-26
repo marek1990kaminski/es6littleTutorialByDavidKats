2aeb3b2903ad46c89cd4599b130ff86495e6e259
Marek K <marekkaminski26@gmail.com>
2017-11-26 12:00:07 +0100

    now we have a little weather app

--- app/index.js
+++ app/index.js
@@ -1,17 +1,37 @@
-import Entity from './entity';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-class Hobbit extends Entity {
-    constructor(name, height) {
-        super(name, height);
+const api_key = "0e05821128c5e8dc296c6b23beac62ba";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: 'Warsaw',
+            description: ''
+        }
     }
 
-    greet() {
-        console.log(`Hello, I am ${this.name} and I am from Shire`);
+    componentDidMount() {
+        this.grabWeather(this.state.city);
+    }
+
+    grabWeather(city) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`).then(response => response.json()).then(json => {
+            console.log(json);
+            let info = "temperature : "+ (Math.round((json.main.temp-273)*10)/10) + ", pressure: "+ json.main.pressure +", wind: " + json.wind.speed + ", clouds: "+ json.weather[0].description;
+            this.setState({description: info})
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Weather Report for {this.state.city}</h1>
+                <h2>{this.state.description}</h2>
+            </div>
+        )
     }
 }
 
-var Frodo = new Hobbit('Frodo', 4.5);
-
-console.log(Entity);
-
-Frodo.greet();
+ReactDOM.render(<App/>, document.getElementById('root'));
